a7d6d7166e239a8949bb4a737c51858ad6dfb3e7
rho <claytonstuart0@gmail.com>
2021-03-23 15:24:08 -0500

    now bot is a class wow

--- bot.py
+++ bot.py
@@ -4,23 +4,27 @@ import switchboard
 import utils
 import core
 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = "irc.irchighway.net"
 channel = "#fart"
 botnick = "rhobot"
 adminname = "rhorama"
+class Bot:
+    def __init__(self, server, channel, botnick, adminname):
+        self.server = server
+        self.channel = channel
+        self.botnick = botnick
+        self.adminname = adminname
+        
+        
 
+    def run(self):
+        ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ircsock.connect((server, 6660))
+        ircsock.send(f"USER {botnick} {botnick} {botnick} {botnick}\n".encode())
+        ircsock.send(f"NICK {botnick}\n".encode())
+        time.sleep(2)
+        utils.joinchan(channel, ircsock)
+        core.main(ircsock)
 
-
-ircsock.connect((server, 6660))
-ircsock.send(f"USER {botnick} {botnick} {botnick} {botnick}\n".encode())
-ircsock.send(f"NICK {botnick}\n".encode())
-time.sleep(2)
-utils.joinchan(channel, ircsock)
-
-
-def main():
-    core.main(ircsock)
-
-
-main()
+bot1 = Bot(server, channel, botnick, adminname)
+bot1.run()

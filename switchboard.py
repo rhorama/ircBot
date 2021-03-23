79b343c6385298c6e245ad9dfe3fdda83d009147
rho <claytonstuart0@gmail.com>
2021-03-22 22:52:09 -0500

    changed the switchboard code to dict lookup

--- switchboard.py
+++ switchboard.py
@@ -3,15 +3,11 @@ import utils
 
 
 def main(input):
-    commandList = ["!w", "!wf", "!cf"]
     if len(input) == 0:
         return "error"
-    for command in commandList:
-        if command in input:
-            if command == commandList[0]:
-                return ircWeatherModule.main(input.split(" ")[1], False)
-            elif command == commandList[1]:
-                return ircWeatherModule.main(input.split(" ")[1], True)
-            else:
-                return utils.flip()
-    return "no command"
+    inputList = input.split(" ")
+    inputCommand = inputList[0]
+    commandDict = {'!w': ircWeatherModule.main(input.split(" ")[1], True), '!wf': ircWeatherModule.main(input.split(" ")[1], True), '!cf': utils.flip()}
+
+    out = commandDict[inputCommand]
+    return out
